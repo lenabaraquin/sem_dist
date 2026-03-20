776d842a07e4fff3acb87ffce794a1ef6c5478e1
Lena Baraquin <baraquin.lena@gmail.com>
2026-03-20 12:01:41 +0100

    première version knn

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 import numpy as np
 import argparse
+import spacy
+
+nlp = spacy.load("fr_core_news_sm")
+
+############################
+### Chargement du corpus ###
+############################
 
 
-def build_ppmi_matrix(corpus: list, window_size: int = 2):
+def load_corpus(corpus_path: str) -> list[str]:
+    """
+    Charge un corpus dans une liste de lemmes étant donné le chemin vers un fichier texte.
+    """
+    with open(corpus_path, "r") as f:
+        corpus = f.read()
+    return tokenize_corpus(corpus)
+
+
+def tokenize_corpus(corpus_str: str) -> list[str]:
+    """
+    Retourne la liste des tokens lemmatisés du corpus.
+    """
+    document = nlp(corpus_str)
+    corpus = [token.lemma_ for token in document]
+    return corpus
+
+
+#####################################
+### Calcul de la matrice des ppmi ###
+#####################################
+
+
+def build_ppmi_matrix(corpus: list[str], window_size: int = 2):
     vocab = list(set(corpus))
     vocab_index = {w: i for i, w in enumerate(vocab)}
     V = len(vocab)
@@ -41,10 +71,47 @@ def build_ppmi_matrix(corpus: list, window_size: int = 2):
     return PPMI, vocab_index
 
 
-def load_corpus(corpus_path: str) -> list:
-    with open(corpus_path, "r") as f:
-        corpus = f.read()
-    return corpus.split()
+##############################
+### Lissage des embeddings ###
+##############################
+
+pass
+
+##########################
+### Similarité cosinus ###
+##########################
+
+
+def cosine_sim(u: np.array, v: np.array) -> np.float64:
+    """
+    Calcule la similarité cosinus entre deux vecteurs.
+    """
+
+    dot_product = np.dot(u, v)
+
+    norm_u = np.linalg.norm(u)
+    norm_v = np.linalg.norm(v)
+
+    return dot_product / (norm_u * norm_v)
+
+
+def find_knn(
+    key_lemma: str, ppmi_matrix: np.array, vocab_index: dict[str, int], k: int = 10
+) -> list[str]:
+    """
+    C'est cracra mais retourne la liste des k lemmes les plus proches du lemme cible dans l'espace des plongements.
+    """
+    key_ppmi = ppmi_matrix[vocab_index[key_lemma]]
+    sim_dict = dict()
+    for lemma, index in vocab_index.items():
+        similarity = cosine_sim(key_ppmi, ppmi_matrix[index])
+        sim_dict[lemma] = similarity
+
+    knn = [
+        lemma
+        for (lemma, _) in sorted(sim_dict.items(), key=lambda x: x[1], reverse=True)
+    ]
+    return knn[:k]
 
 
 def main():
@@ -64,8 +131,9 @@ def main():
 
     ppmi_matrix, vocab_index = build_ppmi_matrix(corpus, 2)
 
-    __import__("pprint").pprint(ppmi_matrix)
-    print(vocab_index)
+    knn = find_knn("campagne", ppmi_matrix, vocab_index, 10)
+
+    __import__("pprint").pprint(knn)
 
 
 if __name__ == "__main__":
